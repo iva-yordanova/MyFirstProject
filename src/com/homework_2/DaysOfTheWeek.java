1edb8dd497523586d1af593dbf966da47f52822b
iva-yordanova <yordanovaa.iva@gmail.com>
2022-04-10 19:20:26 +0300

    homework2

--- src/com/homework_2/DaysOfTheWeek.java
+++ src/com/homework_2/DaysOfTheWeek.java
@@ -2,6 +2,23 @@ package com.homework_2;
 
 public class DaysOfTheWeek {
     public static void main(String[] args) {
-        System.out.println("Test");
+        int i=5;
+        switch(i)
+        {
+            case 1:
+                System.out.println("Case1 ");
+                break;
+            case 2:
+                System.out.println("Case2 ");
+                break;
+            case 3:
+                System.out.println("Case3 ");
+                break;
+            default:
+                System.out.println("Default ");
+        }
+
+
     }
 }
+
