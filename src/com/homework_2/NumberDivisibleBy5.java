dfc8d59510564fe1258f2991b51771764a124473
iva-yordanova <yordanovaa.iva@gmail.com>
2022-04-16 23:35:06 +0300

    Homework 2 - Task 6 (Rename)

--- src/com/homework_2/NumberDivisibleBy5.java
+++ src/com/homework_2/NumberDivisibleBy5.java
@@ -1,6 +1,6 @@
 package com.homework_2;
 
-public class numberDivisibleBy5 {
+public class NumberDivisibleBy5 {
     public static void main(String[] args) {
         int arr[] = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
 
