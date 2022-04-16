2bfb042f2b8d83121609bdb5a78d0a01d4ff904d
iva-yordanova <yordanovaa.iva@gmail.com>
2022-04-16 23:18:17 +0300

    Homework 2 - Task 6 (Number divisible by 5 )

---
+++ src/com/homework_2/numberDivisibleBy5.java
@@ -0,0 +1,18 @@
+package com.homework_2;
+
+public class numberDivisibleBy5 {
+    public static void main(String[] args) {
+        int arr[] = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
+
+        for (int i = 0; i <= arr.length; i++) {
+            if (arr[i] > 150) {
+                break;
+            }
+            if (arr[i] % 5 == 0) {
+                System.out.println(arr[i]);
+
+            }
+        }
+
+    }
+}
